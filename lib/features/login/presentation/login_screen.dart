965cdb6c960e358f0b614f29aa36e91777f8dc59
elmohamdi <elmohamdi.77@gmail.com>
2026-05-08 20:40:46 +0300

    - added spacing file for sized box. - added some colors and text styles. - added font weight helper class. - added general text Button and general text form field. - finished login screen design. - added Two Text widgets(Terms and Conditions and dont have account).

--- lib/features/login/presentation/login_screen.dart
+++ lib/features/login/presentation/login_screen.dart
@@ -1,16 +1,85 @@
+import 'package:doc_advance/core/helpers/spacing.dart';
+import 'package:doc_advance/core/theming/styles.dart';
+import 'package:doc_advance/core/widgets/app_text_button.dart';
+import 'package:doc_advance/core/widgets/app_text_form_field.dart';
+import 'package:doc_advance/features/login/presentation/widgets/dont_have_account_text.dart';
+import 'package:doc_advance/features/login/presentation/widgets/terms_and_conditions_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
 
+final class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  bool isObesecureText = true;
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar:AppBar(
-        title: Text('Login Screen'),
-      ) ,
-      body: Center(
-        child: Text('Login Screen'),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Welcome Back', style: TextStyles.font24BlueBold),
+                verticalSpace(8),
+                Text(
+                  'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
+                  style: TextStyles.font17grayRegular.copyWith(height: 1.5.h),
+                ),
+                verticalSpace(36),
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      AppTextFormField(hintText: 'Email'),
+                      verticalSpace(18),
+                      AppTextFormField(
+                        hintText: 'Password',
+                        isObsecureText: isObesecureText,
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isObesecureText = !isObesecureText;
+                            });
+                          },
+                          child: Icon(
+                            isObesecureText
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                        ),
+                      ),
+                      verticalSpace(24),
+                      Align(
+                        alignment: AlignmentDirectional.centerEnd,
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyles.font13BlueRegular,
+                        ),
+                      ),
+                      verticalSpace(40),
+                      AppTextButton(
+                        buttonText: 'Login',
+                        textStyle: TextStyles.font16whiteSemiBold,
+                        onPressed: () {},
+                      ),
+                      verticalSpace(16),
+                      const TermsAndConditionsText(),
+                      verticalSpace(60),
+                      const DontHaveAccountText(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/features/onboarding/widgets/doc_logo_and_name.dart
+++ lib/features/onboarding/widgets/doc_logo_and_name.dart
@@ -12,7 +12,7 @@ class DocLogoAndName extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SvgPicture.asset('assets/svgs/logo.svg'),
-        SizedBox(child: Text('Docdoc', style: TextStyles.font24Black700weight)),
+        SizedBox(child: Text('Docdoc', style: TextStyles.font24BlackBold)),
       ],
     );
   }
